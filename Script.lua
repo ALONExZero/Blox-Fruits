c373b9ecdec880a1ed20cc207f75f0a834bf89fb
ALONExZero <120350141+ALONExZero@users.noreply.github.com>
2022-12-12 09:38:47 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,3 +3,12 @@ warn("Ur Gay"..math.huge.."%")
 end
 wait(10)
 game.Players.LocalPlayer:kick("Gay Can't Excute This Script")
+spawn(function()
+    while task.wait() do
+        getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
+            if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+                game:GetService("TeleportService"):Teleport(game.PlaceId)
+            end
+        end)
+    end
+end)
